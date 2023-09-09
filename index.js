3525d4e59430d8ae81f1f2376888fd8ebdfcbdd5
Habibur Rahman <freelancerr.habibur@gmail.com>
2023-09-09 06:22:02 +0600

    basic server site added

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.Port || 3000;
+
+const categories = require('./data/categories.json');
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('hellow world');
+})
+
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
+})
+
+app.listen(port, () => {
+    console.log(`Dragon API is running on port: ${port}`)
+})
